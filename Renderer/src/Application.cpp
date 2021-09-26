8d45399616b69901409668d7bd1a327349d37fd0
Chewico <chewico3d@gmail.com>
2021-09-26 20:14:37 +0200

    Initialize

---
+++ Renderer/src/Application.cpp
@@ -0,0 +1,52 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+        return -1;
+
+    float Verices[]{
+        -.5f, -.5f,
+        0, .5f,
+        .5f, -.5f
+
+    };
+
+    unsigned int BufferID;
+    glCreateBuffers(1, &BufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, BufferID);
+    glBufferData(GL_ARRAY_BUFFER, );
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
